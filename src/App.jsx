0844ee0f1dd9b5513d3407a334319f150a9d8e83
galateaxixixiix <akbardermawan1998@gmail.com>
2025-12-03 16:30:29 +0700

    03/12/2025

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 1000);
+    const timer = setTimeout(() => setLoading(false), 3000);
     return () => clearTimeout(timer);
   }, []);
   return loading ? (
--- src/components/home/Project.jsx
+++ src/components/home/Project.jsx
@@ -1,70 +1,271 @@
 import React from "react";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import { MdWebAsset } from "react-icons/md";
+import { FaArrowRightToBracket } from "react-icons/fa6";
 
 const Project = () => {
   return (
-    <div className="pt-2 bg-white/5 text-white">
+    <div className="pt-2 bg-white/5 text-white ">
       {/*  */}
-      <div className="">
-        <div className="m-2">
-          <div>Img</div>
-          <h2> eCommerce App</h2>
-          <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
-          <div>
-            <a href="">
-              <button>view Code </button>
-            </a>
+
+      <div className="w-full h-[700px] md:h-[800px] lg:h-[700px] m-2 md:m-5 lg:pl-14 mx-auto p-2 pl-4  flex justify-start items-center flex-col">
+        <h3 className="text-3xl md:text-4xl lg:text-5xl font-semibold">
+          eCommerce
+        </h3>
+        <h5 className="text-xs md:text-sm xl:text-base text-white mb-4">
+          Asp.Net Core | Blazore | PostgreSQL
+        </h5>
+        <div className="flex flex-col lg:flex-row gap-6">
+          {/* Image */}
+          <div className="w-[95%] md:w-[95%] xl:w-[55%]">
+            <img
+              src="/img/1.JPG"
+              alt="ecommerce"
+              className="w-full h-auto rounded"
+            />
+          </div>
+
+          {/* Text Content */}
+          <div className="w-full md:w-full xl:w-[45%] flex flex-col justify-center gap-6 p-2">
+            {/* Link Demo & Website */}
+            <div className="flex items-center gap-4">
+              <a
+                href="#"
+                className="flex items-center gap-2 text-blue-600 hover:text-blue-700"
+              >
+                <YouTubeIcon className="text-2xl" />
+                <span>See Demo</span>
+              </a>
+
+              <a
+                href="#"
+                className="flex items-center gap-2 text-blue-600 hover:text-blue-700"
+              >
+                <MdWebAsset className="text-2xl" />
+                <span>Visit Web</span>
+              </a>
+            </div>
+
+            {/* Description */}
+            <p className="mb-4 text-gray-700">
+              This e-commerce website makes it easier to conduct buying and
+              selling transactions anywhere.
+            </p>
+
+            {/* Buttons Backend & Frontend */}
+            <div className="flex items-center gap-4">
+              <a href="#">
+                <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+                  Backend Code
+                </button>
+              </a>
+
+              <a href="#">
+                <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+                  Frontend Code
+                </button>
+              </a>
+            </div>
           </div>
         </div>
-        <div>DoktrMe App</div>
-        <div>Stock Market App</div>
       </div>
+
       {/*  */}
-      <div className="bg-black">
-        <div className="m-2">
-          <div>Img</div>
-          <h2> eCommerce App</h2>
-          <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
-          <div>
-            <a href="">
-              <button>view Code </button>
-            </a>
+      <div className="bg-black w-full h-[700px] md:h-[800px] lg:h-[700px] mx-auto p-2 pl-4 md:p-0 md:pl-7 lg:p-16 flex justify-start items-center flex-col">
+        <h3 className="text-3xl md:text-4xl lg:text-5xl font-semibold">
+          eCommerce
+        </h3>
+        <h5 className="text-xs md:text-sm xl:text-base text-white mb-4">
+          Asp.Net Core | Blazore | PostgreSQL
+        </h5>
+        <div className="flex flex-col lg:flex-row gap-6">
+          {/* Image */}
+          <div className="w-[95%] md:w-[95%] xl:w-[55%]">
+            <img
+              src="/img/1.JPG"
+              alt="ecommerce"
+              className="w-full h-auto rounded"
+            />
+          </div>
+
+          {/* Text Content */}
+          <div className="w-full md:w-full xl:w-[45%] flex flex-col justify-center gap-6 p-2">
+            {/* Link Demo & Website */}
+            <div className="flex items-center gap-4">
+              <a
+                href="#"
+                className="flex items-center gap-2 text-blue-600 hover:text-blue-700"
+              >
+                <YouTubeIcon className="text-2xl" />
+                <span>See Demo</span>
+              </a>
+
+              <a
+                href="#"
+                className="flex items-center gap-2 text-blue-600 hover:text-blue-700"
+              >
+                <MdWebAsset className="text-2xl" />
+                <span>Visit Web</span>
+              </a>
+            </div>
+
+            {/* Description */}
+            <p className="mb-4 text-gray-700">
+              This e-commerce website makes it easier to conduct buying and
+              selling transactions anywhere.
+            </p>
+
+            {/* Buttons Backend & Frontend */}
+            <div className="flex items-center gap-4">
+              <a href="#">
+                <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+                  Backend Code
+                </button>
+              </a>
+
+              <a href="#">
+                <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+                  Frontend Code
+                </button>
+              </a>
+            </div>
           </div>
         </div>
-        <div>DoktrMe App</div>
-        <div>Stock Market App</div>
       </div>
       {/*  */}
-      <div className="">
-        <div className="m-2">
-          <div>Img</div>
-          <h2> eCommerce App</h2>
-          <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
-          <div>
-            <a href="">
-              <button>view Code </button>
-            </a>
+      <div className="w-full h-[700px] md:h-[800px] lg:h-[700px] m-2 md:m-5 lg:pl-14 mx-auto p-2 pl-4 flex justify-start items-center flex-col">
+        <h3 className="text-3xl mt-5 md:text-4xl lg:text-5xl font-semibold">
+          eCommerce
+        </h3>
+        <h5 className="text-xs md:text-sm xl:text-base text-white mb-4">
+          Asp.Net Core | Blazore | PostgreSQL
+        </h5>
+        <div className="flex flex-col lg:flex-row gap-6">
+          {/* Image */}
+          <div className="w-[95%] md:w-[95%] xl:w-[55%]">
+            <img
+              src="/img/1.JPG"
+              alt="ecommerce"
+              className="w-full h-auto rounded"
+            />
+          </div>
+
+          {/* Text Content */}
+          <div className="w-full md:w-full xl:w-[45%] flex flex-col justify-center gap-6 p-2">
+            {/* Link Demo & Website */}
+            <div className="flex items-center gap-4">
+              <a
+                href="#"
+                className="flex items-center gap-2 text-blue-600 hover:text-blue-700"
+              >
+                <YouTubeIcon className="text-2xl" />
+                <span>See Demo</span>
+              </a>
+
+              <a
+                href="#"
+                className="flex items-center gap-2 text-blue-600 hover:text-blue-700"
+              >
+                <MdWebAsset className="text-2xl" />
+                <span>Visit Web</span>
+              </a>
+            </div>
+
+            {/* Description */}
+            <p className="mb-4 text-gray-700">
+              This e-commerce website makes it easier to conduct buying and
+              selling transactions anywhere.
+            </p>
+
+            {/* Buttons Backend & Frontend */}
+            <div className="flex items-center gap-4">
+              <a href="#">
+                <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+                  Backend Code
+                </button>
+              </a>
+
+              <a href="#">
+                <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+                  Frontend Code
+                </button>
+              </a>
+            </div>
           </div>
         </div>
-        <div>DoktrMe App</div>
-        <div>Stock Market App</div>
       </div>
       {/*  */}
-      <div className="bg-black">
-        <div className="m-2">
-          <div>Img</div>
-          <h2> eCommerce App</h2>
-          <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
-          <div>
-            <a href="">
-              <button>view Code </button>
-            </a>
+      <div className="bg-black h-[650px] md:h-[800px] lg:h-[650px] w-full mx-auto pt-5 flex pl-4 md:pl-7 lg:pl-16 justify-start items-center flex-col">
+        <h3 className="text-3xl mt-5 md:text-4xl lg:text-5xl font-semibold">
+          eCommerce
+        </h3>
+        <h5 className="text-xs md:text-sm xl:text-base text-white mb-4">
+          Asp.Net Core | Blazore | PostgreSQL
+        </h5>
+        <div className="flex flex-col lg:flex-row gap-6">
+          {/* Image */}
+          <div className="w-[95%] md:w-[95%] xl:w-[55%]">
+            <img
+              src="/img/1.JPG"
+              alt="ecommerce"
+              className="w-full h-auto rounded"
+            />
+          </div>
+
+          {/* Text Content */}
+          <div className="w-full md:w-full xl:w-[45%] flex flex-col justify-center gap-6 p-2">
+            {/* Link Demo & Website */}
+            <div className="flex items-center gap-4">
+              <a
+                href="#"
+                className="flex items-center gap-2 text-blue-600 hover:text-blue-700"
+              >
+                <YouTubeIcon className="text-2xl" />
+                <span>See Demo</span>
+              </a>
+
+              <a
+                href="#"
+                className="flex items-center gap-2 text-blue-600 hover:text-blue-700"
+              >
+                <MdWebAsset className="text-2xl" />
+                <span>Visit Web</span>
+              </a>
+            </div>
+
+            {/* Description */}
+            <p className="mb-4 text-gray-700">
+              This e-commerce website makes it easier to conduct buying and
+              selling transactions anywhere.
+            </p>
+
+            {/* Buttons Backend & Frontend */}
+            <div className="flex items-center gap-4">
+              <a href="#">
+                <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+                  Backend Code
+                </button>
+              </a>
+
+              <a href="#">
+                <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+                  Frontend Code
+                </button>
+              </a>
+            </div>
           </div>
         </div>
-        <div>DoktrMe App</div>
-        <div>Stock Market App</div>
       </div>
       {/*  */}
-      <div className="bg-black">view More Project</div>
+      <div className="bg-black pl-6 md:pl-10 lg:pl-16">
+        <a
+          href="#"
+          className="flex items-center gap-3 text-blue-600 text-3xl md:text-4xl lg:text-5xl font-semibold hover:text-gray-300 transition-colors"
+        >
+          View More Project
+          <FaArrowRightToBracket className="mt-1" />
+        </a>
+      </div>
     </div>
   );
 };
